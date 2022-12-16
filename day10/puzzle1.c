aa3d5fd5c2910d6ea817e56cf3d77e7015368291
kedymera <kedymera@gmail.com>
2022-12-16 10:21:54 +0000

    day10/puzzle1

--- day10/puzzle1.c
+++ day10/puzzle1.c
@@ -3,26 +3,32 @@
 
 #define BUFFSZ 128
 
+int addsignal(int cycle, int X) {
+    if (cycle % 40 == 20) return cycle*X;
+    return 0;
+}
+
 int main() {
     char buff[BUFFSZ];
     FILE *file = fopen("input.txt", "r");
     if (!file) return 1;
     int X = 1;
-    int cycle = 0;
+    int cycle = 1;
+    int signalsum = 0;
     while (fgets(buff, BUFFSZ, file)) {
         switch (buff[0]) {
             case 'a': // addx
+                signalsum += addsignal(cycle++, X);
+                signalsum += addsignal(cycle++, X);
                 X += atoi(buff+5);
-                cycle += 2;
                 break;
             case 'n': // noop
-                ++cycle;
+                signalsum += addsignal(cycle++, X);
                 break;
             default:
                 printf("invalid instruction\n");
                 exit(1);
         }
-        printf("%s", buff);
-        printf("cycle %d: X=%d\n", cycle, X);
     }
+    printf("signalsum == %d\n", signalsum);
 }
