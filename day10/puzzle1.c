aa4c514ccd3500e20751c6ed6179892f1e1308d4
kedymera <kedymera@gmail.com>
2022-12-16 10:11:56 +0000

    day10/puzzle1: count cycles and calculate value in X register

--- day10/puzzle1.c
+++ day10/puzzle1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BUFFSZ 128
 
@@ -6,8 +7,22 @@ int main() {
     char buff[BUFFSZ];
     FILE *file = fopen("input.txt", "r");
     if (!file) return 1;
+    int X = 1;
+    int cycle = 0;
     while (fgets(buff, BUFFSZ, file)) {
+        switch (buff[0]) {
+            case 'a': // addx
+                X += atoi(buff+5);
+                cycle += 2;
+                break;
+            case 'n': // noop
+                ++cycle;
+                break;
+            default:
+                printf("invalid instruction\n");
+                exit(1);
+        }
         printf("%s", buff);
+        printf("cycle %d: X=%d\n", cycle, X);
     }
-    printf("hello world\n");
 }
