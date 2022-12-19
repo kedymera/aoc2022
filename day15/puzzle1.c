f5912b24111da9c9a197a5d4c567d23ad21448ac
kedymera <kedymera@gmail.com>
2022-12-19 20:47:05 +0000

    day15/puzzle1: read sensor data from file

--- day15/puzzle1.c
+++ day15/puzzle1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define BUFFSZ 128
+#define ROW 2000000
+
+long ReadNextNumber(char **p) {
+    for (; **p; ++*p) {
+        if (**p == '-' || (**p >= '0' && **p <= '9')) {
+            return strtol(*p, p, 10);
+        }
+    }
+    printf("not reached\n");
+    return 0;
+}
+
+void ReadSensor(char buff[BUFFSZ]) {
+    long sx, sy, bx, by;
+    sx = ReadNextNumber(&buff);
+    sy = ReadNextNumber(&buff);
+    bx = ReadNextNumber(&buff);
+    by = ReadNextNumber(&buff);
+    printf("s:[%ld,%ld], b:[%ld,%ld]\n", sx, sy, bx, by);
+}
 
 int main() {
     char buff[BUFFSZ];
@@ -8,6 +29,7 @@ int main() {
     if (!file) return 1;
     while (fgets(buff, BUFFSZ, file)) {
         printf("%s", buff);
+        ReadSensor(buff);
     }
     printf("hello world\n");
 }
