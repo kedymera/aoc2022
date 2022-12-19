4362757ded12d84fd0a741369fcb7859a50533fb
kedymera <kedymera@gmail.com>
2022-12-19 05:09:29 +0000

    day13/puzzle1

--- day13/puzzle1.c
+++ day13/puzzle1.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 
 #define BUFFSZ 256
+#define MIN(a,b) (a) < (b) ? (a) : (b)
 
 struct PacketData {
     struct PacketData *parent;
@@ -77,8 +78,40 @@ struct PacketData *ParsePacket(char *buff) {
     return NULL;
 }
 
-bool AreOrdered(struct PacketData *packet1, struct PacketData *packet2) {
-    return true;
+void NumberToSingleton(struct PacketData *numberdata) {
+    assert(numberdata->number >= 0 && numberdata->list == NULL);
+    numberdata->list = malloc(sizeof(struct PacketData));
+    numberdata->listsz = 1;
+
+    numberdata->list->parent = NULL;
+    numberdata->list->list = NULL;
+    numberdata->list->listsz = 0;
+    numberdata->list->number = numberdata->number;
+
+    numberdata->number = -1;
+}
+
+int PacketCompare(struct PacketData *left, struct PacketData *right) {
+    size_t minlen = MIN(left->listsz, right->listsz);
+    for (size_t i = 0; i < minlen; ++i) {
+        long l = left->list[i].number, r = right->list[i].number;
+        if (l >= 0 && r >= 0) { // 'number' is nonneg if packetdata is number (-1 if list)
+            if (l < r) return 1;
+            else if (l > r) return -1;
+            else continue;
+        }
+
+        // know at least one is a list; convert the other if need be
+        if (l >= 0)
+            NumberToSingleton(left->list + i);
+        if (r >= 0)
+            NumberToSingleton(right->list + i);
+
+        int subres = PacketCompare(left->list + i, right->list + i);
+        if (subres) // was conclusive
+            return subres;
+    }
+    return right->listsz - left->listsz; // accounts for 'running out' clause
 }
 
 int main() {
@@ -100,9 +133,13 @@ int main() {
         PrintPacket(packet2);
         printf("\n");
 
-        if (AreOrdered(packet1, packet2)) {
+        if (PacketCompare(packet1, packet2) > 0) {
             printf("packets are ordered, +%d\n", index);
             indexsum += index;
+        } else if (PacketCompare(packet1, packet2) < 0) {
+            printf("packets are not ordered\n");
+        } else {
+            printf("packets are equivalent\n");
         }
 
         FreePacket(packet1);
