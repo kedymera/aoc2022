0b3d3ff401d66861b85110cd7a2780ba93109b43
kedymera <kedymera@gmail.com>
2022-12-19 22:56:31 +0000

    day15/puzzle2

--- day15/puzzle2.c
+++ day15/puzzle2.c
@@ -13,6 +13,12 @@ struct endpt {
     long x;
     bool left;
 };
+struct sensor {
+    long x;
+    long y;
+    long bx;
+    long by;
+};
 
 int EndPtCompare(const void *endpt1, const void *endpt2) {
     // sort by x value
@@ -43,47 +49,59 @@ long FindIntersections(long cy, long r, long ly) {
     }
 }
 
-void ReadSensor(char buff[BUFFSZ], struct endpt **endpts, int *numendpts, long row) {
-    long sx, sy, bx, by;
-    sx = ReadNextNumber(&buff);
-    sy = ReadNextNumber(&buff);
-    bx = ReadNextNumber(&buff);
-    by = ReadNextNumber(&buff);
+void ReadSensor(char buff[BUFFSZ], struct sensor **sensors, int *numsensors) {
+    *sensors = realloc(*sensors, ++*numsensors * sizeof(struct sensor));
+    (*sensors)[*numsensors-1].x = ReadNextNumber(&buff);
+    (*sensors)[*numsensors-1].y = ReadNextNumber(&buff);
+    (*sensors)[*numsensors-1].bx = ReadNextNumber(&buff);
+    (*sensors)[*numsensors-1].by = ReadNextNumber(&buff);
+}
 
-    long radius = l1metric(sx,sy,bx,by);
+void AnalyseSensors(struct sensor *sensors, int numsensors, struct endpt **endpts, int *numendpts, long row) {
+    for (int i = 0; i < numsensors; ++i) {
+        long radius = l1metric(sensors[i].x,sensors[i].y,sensors[i].bx,sensors[i].by);
 
-    long dx = FindIntersections(sy, radius, row);
-    if (dx != -1) {
-        *numendpts += 2;
-        *endpts = realloc(*endpts, *numendpts * sizeof(struct endpt));
-        (*endpts)[*numendpts-2].x = sx-dx;
-        (*endpts)[*numendpts-2].left = true;
-        (*endpts)[*numendpts-1].x = sx+dx;
-        (*endpts)[*numendpts-1].left = false;
+        long dx = FindIntersections(sensors[i].y, radius, row);
+        if (dx != -1) {
+            *numendpts += 2;
+            *endpts = realloc(*endpts, *numendpts * sizeof(struct endpt));
+            (*endpts)[*numendpts-2].x = sensors[i].x-dx;
+            (*endpts)[*numendpts-2].left = true;
+            (*endpts)[*numendpts-1].x = sensors[i].x+dx;
+            (*endpts)[*numendpts-1].left = false;
+        }
+
     }
 }
 
 int main() {
     char buff[BUFFSZ];
+    struct sensor *sensors = NULL;
+    int numsensors = 0;
+
+    FILE *file = fopen("input.txt", "r");
+    if (!file) return 1;
+    while (fgets(buff, BUFFSZ, file)) {
+        ReadSensor(buff, &sensors, &numsensors);
+    }
+    fclose(file);
+    
+    for (int i = 0; i < numsensors; ++i) {
+        printf("sensors [%ld,%ld] -> beacon [%ld, %ld]\n", sensors[i].x, sensors[i].y, sensors[i].bx, sensors[i].by);
+    }
 
     for (long row = 0; row <= 4000000; ++row) {
         printf("row==%ld\n", row);
         struct endpt *endpts = NULL;
         int numendpts = 0;
 
-        FILE *file = fopen("input.txt", "r");
-        if (!file) return 1;
-        while (fgets(buff, BUFFSZ, file)) {
-            ReadSensor(buff, &endpts, &numendpts, row);
-        }
-        fclose(file);
+        AnalyseSensors(sensors, numsensors, &endpts, &numendpts, row);
         
         qsort(endpts, numendpts, sizeof(struct endpt), &EndPtCompare);
 
         int nesting = 0;
         long currleft;
         long lastright = INT_MIN;
-        long unionlength = 0;
         for (int i = 0; i < numendpts; ++i) {
             if (nesting == 0) {
                 assert(endpts[i].left);
@@ -92,7 +110,11 @@ int main() {
                 if (lastright < currleft - 1
                     && SEARCHMIN <= currleft-1 && currleft-1 <= SEARCHMAX
                     && SEARCHMIN <= row && row <= SEARCHMAX) { // a clear gap
-                    printf("potentially at [%ld, %ld]\n", currleft - 1, row);
+                    printf("unknown beacon at [%ld, %ld]\n", currleft - 1, row);
+                    printf("tuning frequency is %ld\n", 4000000*(currleft-1) + row);
+                    free(endpts);
+                    free(sensors);
+                    exit(0);
                 }
             }
 
@@ -105,7 +127,7 @@ int main() {
                 lastright = endpts[i].x;
             }
         }
-        
         free(endpts);
     }
+    free(sensors);
 }
