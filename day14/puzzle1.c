0ee8f2f2a7ac8d29b05d611c0d757b8e49261429
kedymera <kedymera@gmail.com>
2022-12-19 18:35:52 +0000

    day14/puzzle1: read coords in from file

--- day14/puzzle1.c
+++ day14/puzzle1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define BUFFSZ 128
+#define BUFFSZ 1024
+#define XMIN 440
+#define XMAX 510
+#define YMIN 0
+#define YMAX 504
 
 int main() {
     char buff[BUFFSZ];
@@ -8,6 +13,16 @@ int main() {
     if (!file) return 1;
     while (fgets(buff, BUFFSZ, file)) {
         printf("%s", buff);
+        for (char *p = buff; *p; ++p) {
+            if (*p >= '0' && *p <= '9') {
+                long x = strtol(p, &p, 10);
+                // p now on comma after x value
+                ++p;
+                long y = strtol(p, &p, 10);
+                printf("[%ld, %ld]->", x, y);
+            }
+        }
+        printf("\n");
     }
     printf("hello world\n");
 }
