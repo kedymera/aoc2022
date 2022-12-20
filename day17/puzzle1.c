c02726807bf81d871d366ca777bebf8cf0720f98
kedymera <kedymera@gmail.com>
2022-12-20 20:54:46 +0000

    day17/puzzle1: make tower of 2x2s by dropping straight down

--- day17/puzzle1.c
+++ day17/puzzle1.c
@@ -10,6 +10,10 @@
 
 #define WIDTH 7
 
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
 int c(int x, int y) {
     return y*WIDTH + x;
 }
@@ -36,14 +40,48 @@ int main() {
 #endif
     if (!file) return 1;
     fgets(buff, BUFFSZ, file);
-    printf("%s", buff);
+    int jetlen;
+    for (jetlen = 0; buff[jetlen] != '\n'; ++jetlen);
 
-    char *chamber = malloc(10*WIDTH * sizeof(char));
-    memset(chamber, '.', 10*WIDTH);
-    for (int i = 0; i < 10; ++i) {
-        chamber[i] = '0' + i;
+    int height = 10;
+    char *chamber = malloc(height*WIDTH * sizeof(char));
+    memset(chamber, '.', height*WIDTH);
+    PrintChamber(chamber, height);
+
+    int highest = -1; // y coord of highest landed rock (or -1 for floor)
+    int i = 0; // where we are in the jet sequence
+    int rocks = 0; // how many rocks have settled
+    int rockx = 2, rocky = highest + 4, rockh = 2;
+    while (rocks < 2022) {
+        // rock move down
+        if (rocky == 0 || chamber[c(rockx, rocky - 1)] == '#' || chamber[c(rockx + 1, rocky - 1)] == '#') {
+            // settle rock
+            chamber[c(rockx, rocky)] = '#';
+            chamber[c(rockx+1, rocky)] = '#';
+            chamber[c(rockx, rocky+1)] = '#';
+            chamber[c(rockx+1, rocky+1)] = '#';
+            ++rocks;
+            rockx = 2;
+            highest = max(highest, rocky + rockh - 1);
+            if (highest > height - 10) {
+                int newheight = height * 2;
+                chamber = realloc(chamber, newheight*WIDTH * sizeof(char));
+                for (; height < newheight; ++height) {
+                    for (int i = 0; i < WIDTH; ++i) {
+                        chamber[c(i, height)] = '.';
+                    }
+                }
+            }
+            rocky = highest + 4;
+        } else {
+            --rocky;
+        }
+        ++i;
+        i %= jetlen;
     }
-    PrintChamber(chamber, 10);
+
+    PrintChamber(chamber, height);
+    printf("tower is of height %d\n", highest + 1);
 
     free(chamber);
     printf("hello world\n");
