f61129d9927928e126120137509dba97cf5edc9a
kedymera <kedymera@gmail.com>
2022-12-20 20:13:11 +0000

    day17/puzzle1: create chamber

--- day17/puzzle1.c
+++ day17/puzzle1.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifdef SMOL
 #define BUFFSZ 128
 #else
 #define BUFFSZ 10100
 #endif
+
+#define WIDTH 7
+
+int c(int x, int y) {
+    return y*WIDTH + x;
+}
+
+void PrintChamber(char *chamber, int height) {
+    for (int j = height - 1; j >= 0; --j) {
+        printf("|");
+        for (int i = 0; i < WIDTH; ++i) {
+            printf("%c", chamber[c(i, j)]);
+        }
+        printf("|\n");
+    }
+    printf("+");
+    for (int i = 0; i < WIDTH; ++i) printf("-");
+    printf("+\n");
+}
 
 int main() {
     char buff[BUFFSZ];
@@ -15,8 +35,16 @@ int main() {
     FILE *file = fopen("input.txt", "r");
 #endif
     if (!file) return 1;
-    while (fgets(buff, BUFFSZ, file)) {
-        printf("%s", buff);
+    fgets(buff, BUFFSZ, file);
+    printf("%s", buff);
+
+    char *chamber = malloc(10*WIDTH * sizeof(char));
+    memset(chamber, '.', 10*WIDTH);
+    for (int i = 0; i < 10; ++i) {
+        chamber[i] = '0' + i;
     }
+    PrintChamber(chamber, 10);
+
+    free(chamber);
     printf("hello world\n");
 }
