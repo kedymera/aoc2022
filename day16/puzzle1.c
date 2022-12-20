f1794c3902d94ec5ee94a84f9d8795f4ce1ad20d
kedymera <kedymera@gmail.com>
2022-12-20 11:26:29 +0000

    template: change to using cc -D option for choosing smol vs. full input

--- day16/puzzle1.c
+++ day16/puzzle1.c
@@ -5,11 +5,11 @@
 
 int main() {
     char buff[BUFFSZ];
-    FILE *file;
-    if (getenv("SMOL"))
-        file = fopen("inputsmol.txt", "r");
-    else
-        file = fopen("input.txt", "r");
+#ifdef SMOL
+    FILE *file = fopen("inputsmol.txt", "r");
+#else
+    FILE *file = fopen("input.txt", "r");
+#endif
     if (!file) return 1;
     while (fgets(buff, BUFFSZ, file)) {
         printf("%s", buff);
