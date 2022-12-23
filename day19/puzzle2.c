f5ee04f465485ebbd0edcf025cb5c3de52cd7182
kedymera <kedymera@gmail.com>
2022-12-23 22:36:30 +0000

    day19/puzzle2

---
+++ day19/puzzle2.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define BUFFSZ 256
+
+#define TOTTIME 32
+int id, ore_orecost, clay_orecost, obs_orecost, obs_claycost, geode_orecost, geode_obscost, maxorecost;
+long calls = 0;
+int MAXGEODES = 0;
+int maxgeodesproduct = 1;
+
+int next_number(char **p) {
+    for (char *q = *p; *q; ++q) {
+        if (*q >= '0' && *q <= '9') {
+            int a = strtol(q, &q, 10);
+            *p = q;
+            return a;
+        }
+    }
+    assert(0);
+    return -1;
+}
+
+int max(int a, int b) {
+    return a>b?a:b;
+}
+
+int MaximiseGeodes(int time,
+        int ore, int clay, int obs, int geode,
+        int orerobot, int clayrobot, int obsrobot, int geoderobot) {
+    MAXGEODES = max(MAXGEODES, geode);
+    ++calls;
+    if (time <= 0) return geode;
+    if (time <= 1) return geode+geoderobot;
+    // OPTIMISATION: if current branch has no hope of beating the best, even if all remaining time was spent building
+    //                  geoderobots, quit.
+    if (geode + (geoderobot + geoderobot + time - 1) * time / 2 < MAXGEODES) return 0;
+
+    if (time > 24) {
+        for (int i = time; i < TOTTIME; ++i) printf(" ");
+        printf("call%ld (MG%d): o%d+%d c%d+%d O%d+%d g%d+%d\n", calls, MAXGEODES, ore, orerobot, clay, clayrobot, obs, obsrobot, geode, geoderobot);
+    }
+    
+    int maxgeodes = 0;
+    // consider each possible purchase (or no purchase)
+    // conditions are:
+    //   can we afford it?
+    //   do we already make enough of this thing per turn?
+    if (ore >= geode_orecost && obs >= geode_obscost) {
+        maxgeodes = max(maxgeodes, MaximiseGeodes(time-1, ore+orerobot-geode_orecost, clay+clayrobot, obs+obsrobot-geode_obscost, geode+geoderobot, orerobot, clayrobot, obsrobot, geoderobot+1));
+    }
+    if (ore >= obs_orecost && clay >= obs_claycost
+            && obsrobot < geode_obscost) {
+        maxgeodes = max(maxgeodes, MaximiseGeodes(time-1, ore+orerobot-obs_orecost, clay+clayrobot-obs_claycost, obs+obsrobot, geode+geoderobot, orerobot, clayrobot, obsrobot+1, geoderobot));
+    }
+    if (ore >= clay_orecost
+            && clayrobot < obs_claycost) {
+        maxgeodes = max(maxgeodes, MaximiseGeodes(time-1, ore+orerobot-clay_orecost, clay+clayrobot, obs+obsrobot, geode+geoderobot, orerobot, clayrobot+1, obsrobot, geoderobot));
+    }
+    if (ore >= ore_orecost
+            && orerobot < maxorecost) {
+        maxgeodes = max(maxgeodes, MaximiseGeodes(time-1, ore+orerobot-ore_orecost, clay+clayrobot, obs+obsrobot, geode+geoderobot, orerobot+1, clayrobot, obsrobot, geoderobot));
+    }
+    maxgeodes = max(maxgeodes, MaximiseGeodes(time-1, ore+orerobot, clay+clayrobot, obs+obsrobot, geode+geoderobot, orerobot, clayrobot, obsrobot, geoderobot));
+
+    return maxgeodes;
+}
+
+int main() {
+    char buff[BUFFSZ];
+#ifdef SMOL
+    FILE *file = fopen("inputsmol.txt", "r");
+#else
+    FILE *file = fopen("input.txt", "r");
+#endif
+    if (!file) return 1;
+    while (fgets(buff, BUFFSZ, file)) {
+        char *p = buff;
+        id = next_number(&p);
+        ore_orecost = next_number(&p);
+        clay_orecost = next_number(&p);
+        obs_orecost = next_number(&p);
+        obs_claycost = next_number(&p);
+        geode_orecost = next_number(&p);
+        geode_obscost = next_number(&p);
+        maxorecost = max(ore_orecost, max(clay_orecost, max(obs_orecost, geode_orecost)));
+
+        printf("bp %d: orerobot costs %d ore; clayrobot costs %d ore; obsrobot costs %d ore, %d clay; geoderobot costs %d ore, %d obs\n", id, ore_orecost, clay_orecost, obs_orecost, obs_claycost, geode_orecost, geode_obscost);
+
+        MAXGEODES = 0;
+        int maxgeodes = MaximiseGeodes(TOTTIME, 0, 0, 0, 0, 1, 0, 0, 0);
+        printf("max geodes from this blueprint is %d\n", maxgeodes);
+        maxgeodesproduct *= maxgeodes;
+        if (id == 3) break;
+    }
+    printf("maxgeodes product: %d\n", maxgeodesproduct);
+}
